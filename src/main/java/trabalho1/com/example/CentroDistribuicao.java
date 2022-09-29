ef2cf43e903e3503b5be6e642c721f8af980bb92
Tiago Villa <tiago_villa@hotmail.com>
2022-09-29 18:22:03 -0300

    Adiciona implementação de métodos recebeAditivo(), recebeGasolina() e recebeAlcool()

--- src/main/java/trabalho1/com/example/CentroDistribuicao.java
+++ src/main/java/trabalho1/com/example/CentroDistribuicao.java
@@ -1,9 +1,5 @@
 package trabalho1.com.example;
 
-/**
- * Hello world!
- *
- */
 public class CentroDistribuicao {
 
     public enum SITUACAO {
@@ -18,11 +14,21 @@ public class CentroDistribuicao {
     public static final int MAX_ALCOOL = 2500;
     public static final int MAX_GASOLINA = 10000;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+    private int tGasolina;
+    private int tAditivo;
+    private int tAlcool1;
+    private int tAlcool2;
 
     public CentroDistribuicao(int tAditivo, int tGasolina, int tAlcool1, int tAlcool2) {
+        tGasolina = tGasolina < 0 ? 0 : tGasolina;
+        tAditivo = tAditivo < 0 ? 0 : tAditivo;
+        tAlcool1 = tAlcool1 < 0 ? 0 : tAlcool1;
+        tAlcool2 = tAlcool2 < 0 ? 0 : tAlcool2;
+
+        this.tGasolina = tGasolina > MAX_GASOLINA ? MAX_GASOLINA : tGasolina;
+        this.tAditivo = tAditivo > MAX_ADITIVO ? MAX_ADITIVO : tAditivo;
+        this.tAlcool1 = tAlcool1 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2 : tAlcool1;
+        this.tAlcool2 = tAlcool2 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2 : tAlcool2;
     }
 
     public void defineSituacao() {
@@ -33,31 +39,49 @@ public class CentroDistribuicao {
     }
 
     public int gettGasolina() {
-        return 0;
+        return tGasolina;
     }
 
     public int gettAditivo() {
-        return 0;
+        return tAditivo;
     }
 
     public int gettAlcool1() {
-        return 0;
+        return tAlcool1;
     }
 
     public int gettAlcool2() {
-        return 0;
+        return tAlcool2;
     }
 
     public int recebeAditivo(int qtdade) {
-        return 0;
+        if (qtdade < 0) {
+            return -1;
+        }
+        int totalAditivo = tAditivo + qtdade;
+        totalAditivo = totalAditivo > MAX_ADITIVO ? MAX_ADITIVO : totalAditivo;
+
+        return totalAditivo;
     }
 
     public int recebeGasolina(int qtdade) {
-        return 0;
+        if (qtdade < 0) {
+            return -1;
+        }
+        int totalGasolina = tGasolina + qtdade;
+        totalGasolina = totalGasolina > MAX_GASOLINA ? MAX_GASOLINA : totalGasolina;
+
+        return totalGasolina;
     }
 
     public int recebeAlcool(int qtdade) {
-        return 0;
+        if (qtdade < 0) {
+            return -1;
+        }
+        int totalAlcool = tAlcool1 + tAlcool2 + qtdade;
+        totalAlcool = totalAlcool > MAX_ALCOOL ? MAX_ALCOOL : totalAlcool;
+
+        return totalAlcool;
     }
 
     public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto) {
