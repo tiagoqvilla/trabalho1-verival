d504b167e34543edee0463e9e94c22886d110a2f
lucashleal <lucas.hanauer@outlook.com>
2022-10-04 15:12:10 -0300

    Implementado o metodo encomendaCombustivel

--- src/main/java/trabalho1/com/example/CentroDistribuicao.java
+++ src/main/java/trabalho1/com/example/CentroDistribuicao.java
@@ -28,26 +28,23 @@ public class CentroDistribuicao {
 
         this.tGasolina = tGasolina > MAX_GASOLINA ? MAX_GASOLINA : tGasolina;
         this.tAditivo = tAditivo > MAX_ADITIVO ? MAX_ADITIVO : tAditivo;
-        this.tAlcool1 = tAlcool1 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2 : tAlcool1;
-        this.tAlcool2 = tAlcool2 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2 : tAlcool2;
+        if(tAlcool1 > tAlcool2) {
+            this.tAlcool1 = tAlcool2 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2 : tAlcool2;
+            this.tAlcool2 = tAlcool2 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2 : tAlcool2;  
+        } else {
+            this.tAlcool1 = tAlcool1 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2 : tAlcool1;
+            this.tAlcool2 = tAlcool1 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2 : tAlcool1;
+        }
 
+        defineSituacao();
     }
 
     public void defineSituacao() {
-        int tGasolinaAtual = (MAX_GASOLINA - this.tGasolina) / MAX_GASOLINA;
-        int tGasolinaPorcentagem = 100 - (tGasolinaAtual * 100);
-        int tAlcoolAtual = (MAX_ALCOOL - this.tAlcool1) / MAX_ALCOOL;
-        int tAlcoolPorcentagem = 100 - (tAlcoolAtual * 100);
-        int tAditivoAtual = (MAX_ADITIVO - this.tAditivo) / MAX_ADITIVO;
-        int tAditivoPorcentagem = 100 - (tAditivoAtual * 100); 
-        
-        if(tGasolinaPorcentagem >= 50 && tAlcoolPorcentagem >= 50 && tAditivoPorcentagem >= 50) {
-            this.situacao = SITUACAO.NORMAL;
-        } else if(tGasolinaPorcentagem >= 25 && tAlcoolPorcentagem >= 25 && tAditivoPorcentagem >= 25) {
-            this.situacao = SITUACAO.SOBRAVISO;
-        } else {
-            this.situacao =  SITUACAO.EMERGENCIA;
+        int situacao = getSituacaoTanques()[0];
+        for (int i : getSituacaoTanques()) {
+            situacao = i > situacao ? i : situacao;
         }
+        this.situacao = SITUACAO.values()[situacao];
     }
 
     public SITUACAO getSituacao() {
@@ -75,9 +72,9 @@ public class CentroDistribuicao {
             return -1;
         }
         int totalAditivo = tAditivo + qtdade;
-        totalAditivo = totalAditivo > MAX_ADITIVO ? MAX_ADITIVO : totalAditivo;
+        this.tAditivo = totalAditivo > MAX_ADITIVO ? MAX_ADITIVO : totalAditivo;
 
-        return totalAditivo;
+        return tAditivo;
     }
 
     public int recebeGasolina(int qtdade) {
@@ -85,9 +82,9 @@ public class CentroDistribuicao {
             return -1;
         }
         int totalGasolina = tGasolina + qtdade;
-        totalGasolina = totalGasolina > MAX_GASOLINA ? MAX_GASOLINA : totalGasolina;
+        this.tGasolina = totalGasolina > MAX_GASOLINA ? MAX_GASOLINA : totalGasolina;
 
-        return totalGasolina;
+        return this.tGasolina;
     }
 
     public int recebeAlcool(int qtdade) {
@@ -95,12 +92,92 @@ public class CentroDistribuicao {
             return -1;
         }
         int totalAlcool = tAlcool1 + tAlcool2 + qtdade;
-        totalAlcool = totalAlcool > MAX_ALCOOL ? MAX_ALCOOL : totalAlcool;
-
-        return totalAlcool;
+        this.tAlcool1 = totalAlcool / 2 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2: totalAlcool / 2;
+        this.tAlcool2 = totalAlcool / 2 > MAX_ALCOOL / 2 ? MAX_ALCOOL / 2: totalAlcool / 2;
+        return tAlcool1;
     }
 
     public int[] encomendaCombustivel(int qtdade, TIPOPOSTO tipoPosto) {
-        return null;
+        
+        if(qtdade <= 0) {
+            return new int[] { -7, 0, 0, 0, 0 };
+        }
+
+        if(getSituacao() == SITUACAO.EMERGENCIA && tipoPosto == TIPOPOSTO.COMUM) {
+            return new int[] { -14, 0, 0, 0, 0 };
+        }
+
+
+        int qtAditivo = (int) (qtdade * 0.05);
+        int qtGasolina = (int) (qtdade * 0.7);
+        int qtAlcool = (int) (qtdade * 0.25);
+        
+        if(!canRemoveAditivo(qtAditivo)) {
+            return new int[] { -21, 0, 0, 0, 0};
+        }
+
+        if(!canRemoveGasolina(qtGasolina)) {
+            return new int[] { -21, 0, 0, 0, 0};
+        }
+
+        if(!canRemoveAlcool(qtAlcool)) {
+            return new int[] { -21, 0, 0, 0, 0};
+        }
+
+        if(getSituacao() == SITUACAO.SOBRAVISO && tipoPosto == TIPOPOSTO.COMUM) {
+            this.tAditivo -= qtAditivo / 2;
+            this.tGasolina -= qtGasolina / 2;
+            this.tAlcool1 -= qtAlcool / 4;
+            this.tAlcool2 -= qtAlcool / 4;
+        } else if(getSituacao() == SITUACAO.EMERGENCIA && tipoPosto == TIPOPOSTO.ESTRATEGICO) {
+            this.tAditivo -= qtAditivo / 2;
+            this.tGasolina -= qtGasolina / 2;
+            this.tAlcool1 -= qtAlcool / 4;
+            this.tAlcool2 -= qtAlcool / 4;
+        } else {
+            this.tAditivo -= qtAditivo;
+            this.tGasolina -= qtGasolina;
+            this.tAlcool1 -= qtAlcool / 2;
+            this.tAlcool2 -= qtAlcool / 2;
+        }
+
+
+        return new int[] { 0, tAditivo, tGasolina, tAlcool1, tAlcool2};
     }
+
+    private int[] getSituacaoTanques() {
+        return new int[]{getSituacaoCombustivel(this.tGasolina, MAX_GASOLINA), 
+            getSituacaoCombustivel(this.tAlcool1, MAX_ALCOOL / 2), getSituacaoCombustivel(this.tAlcool2, MAX_ALCOOL / 2),
+            getSituacaoCombustivel(this.tAditivo, MAX_ADITIVO)};
+    }
+
+    private int getSituacaoCombustivel(int combustivel, int limiteMaximo) {
+        if(combustivel >= limiteMaximo * 0.5) {
+            return 0;
+        } else if(combustivel >= limiteMaximo * 0.25) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+    private boolean canRemoveAditivo(int qtdade) {
+        return this.tAditivo - qtdade >= 0 ? true : false;
+    }
+
+    private boolean canRemoveGasolina(int qtdade) {
+        return this.tGasolina - qtdade >= 0 ? true : false;
+    }
+
+    private boolean canRemoveAlcool(int qtdade) {
+        return (this.tAlcool1 + this.tAlcool2) - qtdade >= 0 ? true : false;
+    }
+
+
+    public static void main(String[] args) {
+        CentroDistribuicao c = new CentroDistribuicao(249, 4999, 1200, 1200);
+        System.out.println(c.getSituacao());
+    }
+
+
 }
