349ab7fd71a32d71db6c3a5d8e12d49acd5e3155
lucashleal <lucas.hanauer@outlook.com>
2022-10-09 18:27:11 -0300

    Adicionado testes relacionados a encomenda de combustivel

---
+++ src/test/java/trabalho1/com/example/EncomendaCombustivelTest.java
@@ -0,0 +1,143 @@
+package trabalho1.com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import trabalho1.com.example.CentroDistribuicao.SITUACAO;
+import trabalho1.com.example.CentroDistribuicao.TIPOPOSTO;
+
+public class EncomendaCombustivelTest {
+    CentroDistribuicao c = null;
+
+    @BeforeEach
+    void setup() {
+        c = new CentroDistribuicao(0, 0, 0, 0);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "249,4000,2500,ESTRATEGICO"
+    })
+    void faltaAditivo(int aditivo, int gasolina, int alcool, TIPOPOSTO tipoPosto) {
+        c.recebeAditivo(aditivo);
+        c.recebeGasolina(gasolina);
+        c.recebeAlcool(alcool);
+
+        final int resultado[] = c.encomendaCombustivel(5000, tipoPosto);
+        final int resultadoEsperado = -21;
+        final int resultadoObtido = resultado[0];
+        Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "250,3499,2500,ESTRATEGICO"
+    })
+    void faltaGasolina(int aditivo, int gasolina, int alcool, TIPOPOSTO tipoPosto) {
+        c.recebeAditivo(aditivo);
+        c.recebeGasolina(gasolina);
+        c.recebeAlcool(alcool);
+
+        final int resultado[] = c.encomendaCombustivel(5000, tipoPosto);
+        final int resultadoEsperado = -21;
+        final int resultadoObtido = resultado[0];
+        Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "250,4000,2499,ESTRATEGICO"
+    })
+    void faltaAlcool(int aditivo, int gasolina, int alcool, TIPOPOSTO tipoPosto) {
+        c.recebeAditivo(aditivo);
+        c.recebeGasolina(gasolina);
+        c.recebeAlcool(alcool);
+
+        final int resultado[] = c.encomendaCombustivel(5000, tipoPosto);
+        final int resultadoEsperado = -21;
+        final int resultadoObtido = resultado[0];
+        Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "250,4000,2499,ESTRATEGICO"
+    })
+    void qtdadeInvalida(int aditivo, int gasolina, int alcool, TIPOPOSTO tipoPosto) {
+        c.recebeAditivo(aditivo);
+        c.recebeGasolina(gasolina);
+        c.recebeAlcool(alcool);
+
+        final int resultado[] = c.encomendaCombustivel(-1, tipoPosto);
+        final int resultadoEsperado = -14;
+        final int resultadoObtido = resultado[0];
+        Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "350,4000,2500,ESTRATEGICO"
+    })
+    void encomendaValida(int aditivo, int gasolina, int alcool, TIPOPOSTO tipoPosto) {
+        c.recebeAditivo(aditivo);
+        c.recebeGasolina(gasolina);
+        c.recebeAlcool(alcool);
+
+        final int resultado[] = c.encomendaCombustivel(5000, tipoPosto);
+        final boolean qtdAditivo = resultado[1] == 150;
+        final boolean qtdGasolina = resultado[2] == 500;
+        final boolean qtdAlcool = (resultado[3] + resultado[4]) == 1250;
+        Assertions.assertTrue((qtdAditivo && qtdGasolina && qtdAlcool));
+    }
+    
+    @ParameterizedTest
+    @CsvSource({
+        "350,4000,2500,ESTRATEGICO",
+        "350,4000,2500,COMUM"
+    })
+    void encomendaComSituacaoSobraviso(int aditivo, int gasolina, int alcool, TIPOPOSTO tipoPosto) {
+        c.recebeAditivo(aditivo);
+        c.recebeGasolina(gasolina);
+        c.recebeAlcool(alcool);
+
+        final int resultado[] = c.encomendaCombustivel(5000, tipoPosto);
+        final boolean qtdAditivo = tipoPosto == TIPOPOSTO.COMUM ? resultado[1] == 75 : resultado[1] == 150; 
+        final boolean qtdGasolina = tipoPosto == TIPOPOSTO.COMUM ? resultado[2] == 250 : resultado[2] == 500;
+        final boolean qtdAlcool = tipoPosto == TIPOPOSTO.COMUM ? (resultado[3] + resultado[4]) == 1875 : (resultado[3] + resultado[4]) == 1250;
+        Assertions.assertTrue((qtdAditivo && qtdGasolina && qtdAlcool));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "350,2400,2500,ESTRATEGICO",
+    })
+    void encomendaComSituacaoEmergencia(int aditivo, int gasolina, int alcool, TIPOPOSTO tipoPosto) {
+        c.recebeAditivo(aditivo);
+        c.recebeGasolina(gasolina);
+        c.recebeAlcool(alcool);
+
+        final int resultado[] = c.encomendaCombustivel(3000, tipoPosto);
+        final boolean qtdAditivo = resultado[1] == 200; 
+        final boolean qtdGasolina =resultado[2] == 300;
+        final boolean qtdAlcool = (resultado[3] + resultado[4]) == 1750;
+        Assertions.assertTrue((qtdAditivo && qtdGasolina && qtdAlcool));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "350,2400,2500,COMUM",
+    })
+    void encomendaComSituacaoInvalida(int aditivo, int gasolina, int alcool, TIPOPOSTO tipoPosto) {
+        c.recebeAditivo(aditivo);
+        c.recebeGasolina(gasolina);
+        c.recebeAlcool(alcool);
+
+        final int resultado[] = c.encomendaCombustivel(3000, tipoPosto);
+        final int resultadoEsperado = -7;
+        final int resultadoObtido = resultado[0];
+        Assertions.assertEquals(resultadoEsperado, resultadoObtido);
+    }
+}
